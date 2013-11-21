1266c301a0cb72025bc9c4dc98da0720acba3482
Ben Starbird <benlab2001@hotmail.com>
2013-11-21 09:28:06 -0800

    Commitifying Sample

---
+++ UnitTestingViews.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestingViews.Views.Home;
+using RazorGenerator.Testing;
+
+namespace UnitTestingViews.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var view = new Index();
+            Assert.IsNotNull(view.RenderAsHtml());
+        }
+    }
+}
